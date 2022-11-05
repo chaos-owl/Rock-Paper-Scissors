e5c116c9b72550180ebb7d1122b5485859e44730
Kyle Moulton <kylemoulton@pm.me>
2022-11-05 12:41:23 -0400

    Enabled the case sensitive function to work inside playRound - confirmed it is working. added WIn and Lose conditions to playRound - confirmed working

--- Script/script.js
+++ Script/script.js
@@ -11,19 +11,43 @@ function getComputerChoice(){
     }
 
 function playRound(){
+    
     playerSelection = prompt("Choose: Rock, Paper or Scissors");
     getComputerChoice();
+    caseSensitive();
+    console.log(`You chose ${playerSelection} and computer chose ${computerSelection}`);
 
-    return console.log(`You chose ${playerSelection} and computer chose ${computerSelection}`);
+    if (playerSelection === "Rock" && computerSelection === "Scissors"){
+        return console.log(`YOU WIN! ${playerSelection} beats ${computerSelection}!`)
+    }   else if (playerSelection === "Scissors" && computerSelection === "Paper"){
+        return console.log(`YOU WIN! ${playerSelection} beats ${computerSelection}!`)
+    }   else if (playerSelection === "Paper" && computerSelection === "Rock"){
+        return console.log(`YOU WIN! ${playerSelection} beats ${computerSelection}!`)
+    }
+
+    if (playerSelection === "Rock" && computerSelection === "Paper"){
+        return console.log(`YOU LOSE! ${computerSelection} beats ${playerSelection}!`)
+    }   else if (playerSelection === "Paper" && computerSelection === "Scissors"){
+        return console.log(`YOU LOSE! ${computerSelection} beats ${playerSelection}!`)
+    }   else if (playerSelection === "Scissors" && computerSelection === "Rock"){
+        return console.log(`YOU LOSE! ${computerSelection} beats ${playerSelection}!`)
+    }
+
+     
 }
 
 function caseSensitive() {
     firstLetter = playerSelection.charAt(0).toUpperCase();
     lastLetters = playerSelection.slice(1, playerSelection.length).toLowerCase();
-    console.log(firstLetter + lastLetters);
+    playerSelection = firstLetter + lastLetters;
 }
 // CALLS
 playRound();
-caseSensitive();
+
 
 // NOTES
+
+// WINS:
+// R v S, S v P , P v R
+// LOSES:
+// R v P, P v S, S v R
