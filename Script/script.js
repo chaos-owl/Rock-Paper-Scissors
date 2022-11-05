68aee6340508a5a32e1a7dd3b721ff7b54130e0d
Kyle Moulton <kylemoulton@pm.me>
2022-11-05 11:36:27 -0400

    Started working on playRound function - confirms console.log that play around is recieving both player and computer selections

--- Script/script.js
+++ Script/script.js
@@ -2,12 +2,21 @@
 
 const choices = ["Rock", "Paper", "Scissors"];
 let computerSelection = "";
+let playerSelection = "";
+
 // FUNCTIONS
 
 function getComputerChoice(){
     computerSelection = choices [Math.floor(Math.random() * 3)];
     }
 
+function playRound(){
+    playerSelection = prompt("Choose: Rock, Paper or Scissors");
+    getComputerChoice();
+
+    return console.log(`You chose ${playerSelection} and computer chose ${computerSelection}`);
+}
 // CALLS
-getComputerChoice();
+playRound();
+
 // NOTES
